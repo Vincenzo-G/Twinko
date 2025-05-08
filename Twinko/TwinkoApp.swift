bbca6701efddaee81e6fb43e3ef4987a38af6b28
Vincenzo-G <vincenzogerelli@gmail.com>
2025-05-08 13:09:44 +0200

    Initial Commit

---
+++ Twinko/TwinkoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwinkoApp.swift
+//  Twinko
+//
+//  Created by Vincenzo Gerelli on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TwinkoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
